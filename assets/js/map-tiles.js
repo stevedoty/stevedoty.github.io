d545e067a49eb799c5091a4b864fd024306c8e8a
Steve Doty <stevedoty16@gmail.com>
2020-08-23 08:27:45 -0700

    tile generator js connected, css connected. next is to set data and html

--- assets/js/map-tiles.js
+++ assets/js/map-tiles.js
@@ -1,12 +1,31 @@
 var array = [
   {
-    backgroundImg: "",
-    title: "hi",
-    href: "",
-    screenShot: "",
-    paragraph: "",
-    gH: "",
-    gHpages: "",
+    backgroundImg: "images/pic04.jpg",
+    title: "E-Commerce",
+    screenShot: "images/crwn-clothing1.png",
+    gH: "https://stevedoty.github.io/crwn-clothing/",
+    gHpages: "https://github.com/stevedoty/crwn-clothing",
+  },
+  {
+    backgroundImg: "images/pic04.jpg",
+    title: "E-Commerce",
+    screenShot: "images/crwn-clothing1.png",
+    gH: "https://stevedoty.github.io/crwn-clothing/",
+    gHpages: "https://github.com/stevedoty/crwn-clothing",
+  },
+  {
+    backgroundImg: "images/pic04.jpg",
+    title: "E-Commerce",
+    screenShot: "images/crwn-clothing1.png",
+    gH: "https://stevedoty.github.io/crwn-clothing/",
+    gHpages: "https://github.com/stevedoty/crwn-clothing",
+  },
+  {
+    backgroundImg: "images/pic04.jpg",
+    title: "E-Commerce",
+    screenShot: "images/crwn-clothing1.png",
+    gH: "https://stevedoty.github.io/crwn-clothing/",
+    gHpages: "https://github.com/stevedoty/crwn-clothing",
   }
 ];
 
@@ -14,7 +33,7 @@ var array = [
 function createArticle(item, i){
   var newTile = document.createElement('article');
   newTile.class = "style6";
-  var s = '<div id="'+item.title+'"></div>';
+  var s = '<span class="image"><img src="'+item.backgroundImg+'" alt="" /></span>    <a><h2>'+item.title+'</h2><div class="content"><img id="project_screenshot" src="'+item.screenShot+'" alt="" /><p>JS TS <b>|</b> CSS SASS <b>|</b> HTML <b>|</b> REACT REDUX <b>|</b> FIREBASE STRIPE <b>|</b> GIT CLI API</p><button class="tile-button" onclick="goTo('+item.gH+')">Run</button><button class="tile-button" onclick="goTo('+item.gHpages+')">Source</button></div></a>';
   newTile.innerHTML = s;
   return newTile;
 }
@@ -27,17 +46,4 @@ function renderArticleTiles(){
 }
 renderArticleTiles();
 
-// <article class="style4">
-//   <span class="image">
-//     <img src="images/pic04.jpg" alt="" />
-//   </span>
-//   <a>
-//     <h2>E-Commerce</h2>
-//     <div class="content">
-//       <img id="project_screenshot" src="images/crwn-clothing1.png" alt="" />
-//       <p>JS TS <b>|</b> CSS SASS <b>|</b> HTML <b>|</b> REACT REDUX <b>|</b> FIREBASE STRIPE <b>|</b> GIT CLI API</p>
-//       <button class="tile-button" onclick="goTo('https://stevedoty.github.io/crwn-clothing/')">Run</button>
-//       <button class="tile-button" onclick="goTo('https://github.com/stevedoty/crwn-clothing')">Source</button>
-//     </div>
-//   </a>
-// </article>
+// '<article class="style4"><span class="image"><img src="'+images/pic04.jpg+'" alt="" /></span><a><h2>'+E-Commerce+'</h2><div class="content"><img id="project_screenshot" src="images/crwn-clothing1.png" alt="" /><p>JS TS <b>|</b> CSS SASS <b>|</b> HTML <b>|</b> REACT REDUX <b>|</b> FIREBASE STRIPE <b>|</b> GIT CLI API</p><button class="tile-button" onclick="goTo(''+https://stevedoty.github.io/crwn-clothing/+'')">Run</button><button class="tile-button" onclick="goTo(''+https://github.com/stevedoty/crwn-clothing+'')">Source</button></div></a></article>'
